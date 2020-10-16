efdc6ffb560525ebf7a8eb0d9fdec919637015c6
Joshua Jeschek <joshua.jeschek@gmail.com>
2020-10-16 20:16:47 +0200

    Added !invite command to misc.py

--- misc.py
+++ misc.py
@@ -8,5 +8,11 @@ class Miscellaneous(commands.Cog):
     @commands.command()  # !ping
     async def ping(self, ctx):
         ping = round(self.bot.latency * 1000)
-        await ctx.send(f'My current ping: {ping}ms')
-        print(f'>>> Showed my ping({ping}), as requested by {ctx.message.author}')
+        await ctx.send(f'My current ping is: {ping}ms')
+        print(f'>>> Showed my ping({ping}) to {ctx.message.author}')
+
+    @commands.command()  # !invite
+    async def invite(self, ctx):
+        link = 'https://discord.gg/KVHKnFt'
+        await ctx.send(f'Invite people to this server!\n{link}')
+        print(f'>>> Showed invite to {ctx.message.author}')
