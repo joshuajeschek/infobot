ec88d6559d77c8ba345753758ffe3806ea0001ba
vanitasboi <joshua.jeschek@gmail.com>
2021-04-17 01:31:27 +0200

    implemented the status command

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,7 +1,9 @@
 import SetInviteCommand from './admin/setinvite';
 import InviteCommand from './util/invite';
+import StatusCommand from './util/status';
 
 export default [
     InviteCommand,
     SetInviteCommand,
+    StatusCommand,
 ];
