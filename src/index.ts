29383a2aec6a06b987dd0938aa6e0810972706c9
vanitasboi <joshua.jeschek@gmail.com>
2021-04-17 17:49:49 +0200

    removed unused dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import InfoBot from './modules/infobot.js';
 import dotenv from 'dotenv';
+
+/*
+load .env file!
+required values:
+    - PROD_TOKEN
+    - DEV_TOKEN
+    - MONGO_IP
+    - MONGO_PW
+*/
+
 dotenv.config();
 
 import config from '../config.json';
