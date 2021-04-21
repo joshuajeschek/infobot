b00d1fe851f0c33228fea9984453895200a18c2e
vanitasboi <joshua.jeschek@gmail.com>
2021-04-21 17:18:28 +0200

    🔀 merged sendmsg and sendmbd

--- src/commands/admin/sendmessage.ts
+++ src/commands/admin/sendmessage.ts
@@ -1,5 +1,6 @@
-import { Message, TextChannel } from 'discord.js';
+import { MessageEmbed, TextChannel } from 'discord.js';
 import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando';
+import { DownloaderHelper } from 'node-downloader-helper';
 
 interface Args {
     channel: TextChannel,
@@ -28,16 +29,56 @@ export default class SendMessageCommand extends Command {
                     label: 'content',
                     prompt: 'What should the message say?',
                     type: 'string',
+                    default: '',
                 },
             ],
         });
     }
 
-    async run(msg: CommandoMessage, { channel, content }: Args): Promise<Message> {
+    async run(msg: CommandoMessage, { channel, content }: Args): Promise<null> {
         console.log('>>> sendmessage by', msg.author.tag);
 
-        msg.react('✅');
+        if (content === '' && msg.attachments.size === 0) {
+            msg.reply('cannot send an empty message!');
+            return null;
+        }
 
-        return channel.send(content);
+        if (msg.attachments.size != 0) {
+            const url = msg.attachments.first()?.url;
+            const is_json = msg.attachments.first()?.name?.endsWith('.json');
+            if (!url || !is_json) {
+                msg.react('❌');
+                msg.reply('Couldn\'t process the attachment.');
+                return null;
+            }
+
+            const download = new DownloaderHelper(url, 'tmp', {
+                override: true,
+            });
+
+            download.on('end', async () => {
+                const json_path = download.getDownloadPath().replace('\\', '/');
+                import('./../../../' + json_path)
+                    .then(embed => {
+                        msg.react('✅');
+                        channel.send(content, new MessageEmbed(embed));
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        msg.react('❌');
+                        msg.reply('Couldn\'t download the attachment.');
+                    });
+            });
+
+            download.start();
+
+        }
+        else {
+            msg.react('✅');
+            channel.send(content);
+        }
+
+        return null;
+
     }
 }
