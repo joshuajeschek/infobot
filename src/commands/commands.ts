254fa97499020af129560716a3a7077ce4c6edb4
vanitasboi <joshua.jeschek@gmail.com>
2021-04-29 12:32:17 +0200

    ⚙ Added Settings Command

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -4,6 +4,7 @@ import EditMessageCommand from './admin/editmessage';
 import ReactionRoleCommand from './admin/reactionrole';
 import SendMessageCommand from './admin/sendmessage';
 import SetInviteCommand from './admin/setinvite';
+import SettingsCommand from './admin/settings';
 import MenuCommand from './mensa/menu';
 import InviteCommand from './util/invite';
 import StatusCommand from './util/status';
@@ -24,4 +25,5 @@ export const commands = [
     AutoReactCommand,
     AutoExecCommand,
     ReactionRoleCommand,
+    SettingsCommand,
 ];
